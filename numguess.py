eb048b29e58808ca76c6e698ee8d6cf33ff1676e
0andme <yu00mi97@gmail.com>
2023-01-09 16:25:45 +0900

    feat : 사용자 예측값과 정답 비교하여 결과 출력
    사용자의 이름과 예측값과 함께 정답과의 일치 여부 출력

--- numguess.py
+++ numguess.py
@@ -13,3 +13,7 @@ print(f"안녕하세요 {user_name}님")
 # 사용자로부터 예측값 입력 받기
 user_guess=int(input('1부터 100사이의 정답을 추측해주세요 >'))
 
+# 결과 출력 
+print(f"정답은 바로 {answer}였습니다!!")
+print(f"{user_name}님의 예측값 {user_guess}는 과연 정답과 일치했을까요? {user_guess==answer}")
+
