1ced9ca745bfd4667a0e72a84f4d381b47546727
0andme <yu00mi97@gmail.com>
2023-01-10 15:11:52 +0900

    feat : Add comparing user's guess with answer
    사용자 예측값과 정답을 비교
    - 사용자의 값과 정답이 같을 때 축하메시지 출력
    - 사용자의 값이 더 클 때와 작을 때 각각의 메시지 출력

--- numguess.py
+++ numguess.py
@@ -1,8 +1,10 @@
 # randint 기능 import
 from random import randint
+from time import sleep
 
 # 랜덤값 생성
 answer=randint(1,100)
+print(f'{answer}')
 
 # 사용자로부터 이름 입력 받기 
 user_name=input('이름을 입력해주세요>')
@@ -14,6 +16,21 @@ print(f"안녕하세요 {user_name}님")
 user_guess=int(input('1부터 100사이의 정답을 추측해주세요 >'))
 
 # 결과 출력 
-print(f"정답은 바로 {answer}였습니다!!")
-print(f"{user_name}님의 예측값 {user_guess}는 과연 정답과 일치했을까요? {user_guess==answer}")
+# 사용자의 예측값과 정답 비교
+print(f"당신은 {user_guess}를 골랐습니다")
+
+if user_guess==answer:
+    print('----------------------------------------------')
+    sleep(1)
+    print('----------------------------------------------')
+    sleep(1)
+    print('----------------------------------------------')
+    sleep(1)
+    print(f'{user_name}님, 축하드립니다 정답 {answer}를 맞추셨습니다')
+elif user_guess>answer:
+    print(f'{user_guess}는 정답보다 큰 수입니다')
+else: 
+    print(f'{user_guess}는 정답보다 작은 수입니다')
+
+
 
