d53e64e0d49e97acfa9536284ed023790c605c9a
0andme <yu00mi97@gmail.com>
2023-01-11 10:01:27 +0900

    #10 feat : 사용자가 문을 바꾸지 않았을 때의 이긴 횟수 증가
    - 사용자가 문을 바꾸지 않고 그대로 선택했을 경우 그 값이 1이면 이긴 횟수 증가
    
    - 변수명 오타 수정

--- monty_hall.py
+++ monty_hall.py
@@ -3,7 +3,7 @@ from random import shuffle,choice
 repeat_num=5
 doors=[0,0,1]
 user_choice=0
-no_chane_win=0
+no_change_win=0
 change_win=0
 
 
@@ -11,3 +11,5 @@ change_win=0
 for i in range(repeat_num):
     shuffle(doors)
     user_choice=choice(doors)
+    if(user_choice==1):
+        no_change_win+=1
