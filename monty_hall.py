b79bd13dd8cb3e93fcadc162644e9f92281a3070
0andme <yu00mi97@gmail.com>
2023-01-11 10:42:04 +0900

    #6 refactor : 이긴 횟수 저장 변수의 타입 변경
    - no_change_win과 change_win 변수를
    딕셔너리 구조의 변수로 통합

--- monty_hall.py
+++ monty_hall.py
@@ -6,15 +6,19 @@ user_choice=0
 no_change_win=0
 change_win=0
 
+result={
+    'no_change_win':0,
+    'change_win':0,
+}
 
 
 for i in range(repeat_num):
     shuffle(doors)
     user_choice=choice(doors)
     if(user_choice==1):
-        no_change_win+=1
+        result['no_change_win']+=1
     # 사용자가 문을 변경
     else:
-        change_win+=1
-
-print(f"반복 횟수 : {repeat_num} / 문 변경시 이길 확률 : {change_win/repeat_num*100}/ 문 미 변경시 이길 확률: {no_change_win/repeat_num*100}")
+        result['change_win']+=1
+        
+print(f"반복 횟수 : {repeat_num} / 문 변경시 이길 확률 : {result['change_win']/repeat_num*100}/ 문 미 변경시 이길 확률: {result['no_change_win']/repeat_num*100}")
